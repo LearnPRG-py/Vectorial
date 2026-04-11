7048bbfd9d8058118769d9b213a1af455d5725ae
Aryan Krishnan <aryankrishnan4b@gmail.com>
2026-04-11 15:38:40 +0530

    Windows build tool fixes
    Have windows build using choco instead of winget

--- src/install_deps.py
+++ src/install_deps.py
@@ -5,9 +5,9 @@ def install_deps():
     if sys.platform == "darwin":
         subprocess.run(["brew", "install", "cmake", "gcc"], check=True)
     elif sys.platform == "win32":
-        subprocess.run(["winget", "install", "Kitware.CMake", "MinGW.MinGW"], check=True)
+        subprocess.run(["choco", "install", "cmake", "mingw", "-y"], check=True)
     elif sys.platform == "linux":
         subprocess.run(["sudo", "apt", "install", "-y", "cmake", "g++"], check=True)
-    subprocess.run([sys.executable, "-m", "pip", "install", "black==22.3.0"])
+    subprocess.run([sys.executable, "-m", "pip", "install", "black==22.3.0"], check=True)
 
 install_deps()
